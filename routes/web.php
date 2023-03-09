e31e3da88a9b331fd04468615ee5143e34c94235
Evgeny <evgenyglizhin@gmail.com>
2023-03-09 12:18:13 +0200

    Lesson 4, 5

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,15 @@ Route::group(['namespsce' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', 'App\Http\Controllers\Main\IndexController');
 });
 
+Route::group(['namespase' => 'Admin', 'prefix' => 'admin'], function(){
+    Route::group(['namespsce' => 'App\Http\Controllers\Admin\Main'], function () {
+        Route::get('/', 'App\Http\Controllers\Admin\Main\IndexController');
+    });
+    Route::group(['namespace' => 'App\Http\Controllers\Admin\Category', 'prefix' => 'categories'], function () {
+        Route::get('/', 'App\Http\Controllers\Admin\Category\IndexController');
+    });
+
+});
+
 Auth::routes();
 
