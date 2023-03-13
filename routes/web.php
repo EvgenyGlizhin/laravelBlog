478b319092bf9e9810eba81b9969c6d2f264a9a0
Evgeny <evgenyglizhin@gmail.com>
2023-03-13 19:02:05 +0200

    Tag CRUD

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,16 @@ Route::group(['namespase' => 'Admin', 'prefix' => 'admin'], function(){
         Route::delete('/{category}', 'App\Http\Controllers\Admin\Category\DeleteController')->name('admin.category.delete');
 
     });
+    Route::group(['namespace' => 'App\Http\Controllers\Admin\Tag', 'prefix' => 'tags'], function () {
+        Route::get('/', 'App\Http\Controllers\Admin\Tag\IndexController')->name('admin.tag.index');
+        Route::get('/create', 'App\Http\Controllers\Admin\Tag\CreateController')->name('admin.tag.create');
+        Route::post('/', 'App\Http\Controllers\Admin\Tag\StoreController')->name('admin.tag.store');
+        Route::get('/{tag}', 'App\Http\Controllers\Admin\Tag\ShowController')->name('admin.tag.show');
+        Route::get('/{tag}/edit', 'App\Http\Controllers\Admin\Tag\EditController')->name('admin.tag.edit');
+        Route::patch('/{tag}', 'App\Http\Controllers\Admin\Tag\UpdateController')->name('admin.tag.update');
+        Route::delete('/{tag}', 'App\Http\Controllers\Admin\Tag\DeleteController')->name('admin.tag.delete');
 
-
+    });
 });
 
 Auth::routes();
